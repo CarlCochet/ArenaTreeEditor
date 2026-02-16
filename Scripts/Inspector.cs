44f8306a822e9356f7510055fdb806d501e86d4f
Carl <carl.cochet@gmail.com>
2026-02-16 21:18:05 +0100

    Add UI layout

--- Scripts/Inspector.cs
+++ Scripts/Inspector.cs
@@ -3,4 +3,57 @@ using System;
 
 public partial class Inspector : Control
 {
+    [Export] private Button _addNode;
+    [Export] private Button _removeNode;
+    [Export] private Button _linkNodes;
+    [Export] private Button _newTree;
+    [Export] private Button _open;
+    [Export] private Button _save;
+    [Export] private Button _addEffect;
+
+    public override void _Ready()
+    {
+        _addNode.Pressed += AddNode;
+        _removeNode.Pressed += RemoveNode;
+        _linkNodes.Pressed += LinkNodes;
+        _newTree.Pressed += NewTree;
+        _open.Pressed += Open;
+        _save.Pressed += Save;
+        _addEffect.Pressed += AddEffect;
+    }
+
+    private void AddNode()
+    {
+        
+    }
+
+    private void RemoveNode()
+    {
+        
+    }
+
+    private void LinkNodes()
+    {
+        
+    }
+
+    private void NewTree()
+    {
+        
+    }
+    
+    private void Open()
+    {
+        
+    }
+    
+    private void Save()
+    {
+        
+    }
+
+    private void AddEffect()
+    {
+        
+    }
 }
--- Scripts/SphereEditor.cs
+++ Scripts/SphereEditor.cs
@@ -1,6 +1,18 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class SphereEditor : Node2D
 {
+    private Dictionary<int, SphereBoardData> _data;
+
+    public override void _Ready()
+    {
+        
+    }
+    
+    private void LoadData()
+    {
+        _data = new Dictionary<int, SphereBoardData>();
+    }
 }
